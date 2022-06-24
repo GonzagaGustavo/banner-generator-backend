ba252c9688b28c279aa3b9faed0989e15ab81ef9
Gustavo Gonzaga <94642849+Gustavo13132@users.noreply.github.com>
2022-06-24 09:43:59 -0300

    Update connection.js

--- api/connection.js
+++ api/connection.js
@@ -1,17 +1,17 @@
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  database: "banners",
-  user: "lcxx2nyzseq4",
-  host: "np0zi8nzgp2s.aws-sa-east-1-1.psdb.cloud",
-  password: "pscale_pw_XrsM0vHQ4cCg8u7TzfE-7Gjjr17m6Ddk5-eoHLqYT_E",
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
-});
+// const mysql = require("mysql2");
+// const connection = mysql.createConnection({
+//   database: "banners",
+//   user: "lcxx2nyzseq4",
+//   host: "np0zi8nzgp2s.aws-sa-east-1-1.psdb.cloud",
+//   password: "pscale_pw_XrsM0vHQ4cCg8u7TzfE-7Gjjr17m6Ddk5-eoHLqYT_E",
+//   ssl: {
+//     require: true,
+//     rejectUnauthorized: false,
+//   },
+// });
 
-connection.query(
-  "CREATE TABLE IF NOT EXISTS usuarios (id INT NOT NULL AUTO_INCREMENT, nome VARCHAR(60) NOT NULL, email VARCHAR(60) NOT NULL, senha VARCHAR(80) NOT NULL, admin BOOLEAN NOT NULL DEFAULT FALSE, PRIMARY KEY(id)) ENGINE=innoDB"
-);
+// connection.query(
+//   "CREATE TABLE IF NOT EXISTS usuarios (id INT NOT NULL AUTO_INCREMENT, nome VARCHAR(60) NOT NULL, email VARCHAR(60) NOT NULL, senha VARCHAR(80) NOT NULL, admin BOOLEAN NOT NULL DEFAULT FALSE, PRIMARY KEY(id)) ENGINE=innoDB"
+// );
 
-module.exports = connection;
+// module.exports = connection;
