48ef9b35e5aef24b64bc222fe1dcf6d0fe0ea318
LuizFaust <andre.faust@gmail.com>
2022-06-27 13:28:33 -0300

    Add role to token

--- api/Login/utils.js
+++ api/Login/utils.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken')
 const generateToken = (user) => {
     return jwt.sign({
         id: user.id,
-        email: user.email
+        email: user.email,
+        role: user.role
     }, 'Chave Secreta', {
         expiresIn: 60 * 60 * 24 * 365
     })
